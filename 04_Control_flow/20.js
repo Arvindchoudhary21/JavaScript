136579ca856479743db48cd5bddd6fa7a8f63d32
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-09 18:11:05 +0530

    done

---
+++ 04_Control_flow/20.js
@@ -0,0 +1,158 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+
+
+// how to check if array is empty or not 
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+
+
+
+// how to check if object is empty or not by just get object as array and check 
+// length of the array simple 
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10 // assign 5 to val1
+
+// val1 = null ?? 10 //assign 10 to the val1 
+// agar 10 ke jagah par function hota so agar function se kuch
+// value ayi hai then val1 ko assign kro nhi to null assign kr do aise kya hoga ki
+// val1 ko kuch to assign hoga bas isiliye ye ?? function use hota hai
+
+// val1 = undefined ?? 15 // same statement jo uper wala hai null wala 
+
+val1 = null ?? 10 ?? 20 // assign 10 because first valid value 10 hai that's why
+
+console.log(val1);
+
+
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
+
+
+
+// ***********************things you know ok so just read it and move on **********
+
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+
+
+
+
+
+
+
+
+// ********************************
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
