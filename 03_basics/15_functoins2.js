54ff8f4bf1ca87daf75cf42cd2ef3f3b20a300be
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-09 10:58:10 +0530

    done

--- 03_basics/15_functoins2.js
+++ 03_basics/15_functoins2.js
@@ -20,7 +20,7 @@ function newcalculateCartPrice2(val1 ,val2 ,...num)
 }
 console.log(newcalculateCartPrice2(2 ,3 ,4, 5 ,6));//it will return all the values in the 
 // array format and 2 and 3 val1 and val2 me chala gya and rest array me convert ho gya 
-
+// output -> [ 4, 5, 6 ] and 2 and 3 goes to val1 and val2
 
 
 
@@ -33,3 +33,22 @@ function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 handleObject(user) // works fine 
+
+// direct object bhi pass kr sakte hai in the function ok
+handleObject({
+    username :"john",
+    price : 299
+}) // output works fine 
+
+
+
+
+
+
+// also we can pass array to the finction
+const myarray = [1 ,2 , 3 , 4];
+function getarray(arr){
+    return arr[0];
+}
+console.log(getarray(myarray)); // get the 0th element of the array
+console.log(getarray([5,6,7,8])); //we can pass the array also direct
