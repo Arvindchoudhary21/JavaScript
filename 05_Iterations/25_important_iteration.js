ba11cd05d4581447b4ecdb4beec03b1a15bc7239
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-10 01:31:34 +0530

    done

--- 05_Iterations/25_important_iteration.js
+++ 05_Iterations/25_important_iteration.js
@@ -1,4 +1,4 @@
-// iterate on arrays of objects using foreach loop
+// // iterate on arrays of objects using foreach loop
 const myCoding = [
     {
         languageName : "cpp",
@@ -18,8 +18,24 @@ const myCoding = [
     },
 ];
 
-myCoding.forEach( (it) => {
-    console.log(it.languageName);
-    console.log(it.languagefilename);
+// myCoding.forEach( (it) => {
+//     console.log(it.languageName);
+//     console.log(it.languagefilename);
+// } )
+// // simple just use the for each loop and iterate on array of objects 
+
+
+
+// also you can iterate to objects using foreach so dekho for of se object nhi ho 
+// pa rha tha for in se iterate ho rha tha but foreach se dono object and array iterate
+// ho jayega so yhi use krna better hoga 
+const obj = {
+    username : "arvind",
+    location : "kolkata" 
+}
+Object.keys(obj).forEach( (key) => {
+    // console.log(key , obj[key]);
 } )
-// simple just use the for each loop and iterate on array of objects 
+
+
+
