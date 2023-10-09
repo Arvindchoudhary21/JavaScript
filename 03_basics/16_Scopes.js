857f71abc14ab9fde634f9b540aa585d5c9c6758
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-09 11:44:02 +0530

    done

---
+++ 03_basics/16_Scopes.js
@@ -0,0 +1,9 @@
+var c = 300;
+if (true) {
+    c = 3;    
+}
+console.log(c);
+//this is the problem of the var so please ignore this kiuki ye scope ke andar bhi
+// changes le leta hai and fir problem create krta hai
+
+// but ye problem let me nhi hota so use let ok
