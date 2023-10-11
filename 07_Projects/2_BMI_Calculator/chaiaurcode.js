eccdcd8d44edff1060f9d866d0928022de5f13f3
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-11 11:25:12 +0530

    done

--- 07_Projects/2_BMI_Calculator/chaiaurcode.js
+++ 07_Projects/2_BMI_Calculator/chaiaurcode.js
@@ -10,24 +10,48 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   // jo default action hai submit wala wo stop kro uper
-  // wala code ka ye meaning hai ok
+  // wala code ka ye meaning hai ok kiuki form to server par submit hota hai
+  // so wo sab operation nhi krna hai so isiliye wo sab operations ko prevent kro
 
   // ab values chahiye height and weight
+  // ye .value function yad rakhna aise hi kam krta hai for input elements dekho html me hai
+  // values string me milti hai so isko humlogo ko integer me convert krna hoga 
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
+  // result to bas ek element hai so isko bhi store kro simply and isme hi answer store krna hai
   const results = document.querySelector('#results');
-
-  if (height === '' || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please enter valid height`;
-  } else if (weight === '' || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please enter valid weight`;
-  } else {
+  const results2 = document.querySelector('#results2');
+  // if (height === '' || height < 0 || isNaN(height)) {
+  //   results.innerHTML = `Please enter valid height`;
+  // } else if (weight === '' || weight < 0 || isNaN(weight)) {
+  //   results.innerHTML = `Please enter valid weight`;
+  // } else {
+  //   const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  //   // tofixed hai ki decimal ke bad sirf 2 value ayega ok and simple
+  //   // formula hai bmi calculate krne ka ok
+  //   // ab bmi ko result me add kr do
+  //   results.innerHTML = `<span>${bmi}</span>`;
+  //   // simple result me add ho gya
+  // }
+  // ye jyada simple hai uper wala bhi simple hai but ye jyada simple hai  
+  if(height && weight)
+  {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    // tofixed hai ki decimal ke bad sirf 2 value ayega ok and simple
-    // formula hai bmi calculate krne ka ok
-    // ab bmi ko result me add kr do
     results.innerHTML = `<span>${bmi}</span>`;
-    // simple result me add ho gya
+    let text = "";
+    if(bmi <= 18.6){
+      text = "You are under weight";
+    }
+    else if(bmi >= 18.6 && bmi <= 24.9){
+      text = "You are in normal weight";
+    }
+    else{
+      text = "You are over weight";
+    }
+    results2.innerHTML = text;
+  }
+  else{
+    results.innerHTML = `Please enter valid weight`;
   }
 });
     
