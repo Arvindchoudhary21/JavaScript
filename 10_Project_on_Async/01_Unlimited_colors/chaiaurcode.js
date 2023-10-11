ac647d35a6a33c2ec9f71407c1b0b6b498f29f5d
Arvind Choudhary <arvindchoudhary6290@gmail.com>
2023-10-12 02:09:16 +0530

    done

---
+++ 10_Project_on_Async/01_Unlimited_colors/chaiaurcode.js
@@ -0,0 +1,31 @@
+//!need to generate random color
+const randomColor = function () {
+    const hex = '0123456789ABCDEF'; //!THIS IS THE RANGE OF COLORS 0-9 AND A-F
+    let color = '#';
+    // !so from hex 6 values uthana hai wahi permutation dega color ka 
+    // !pick 6 random values from hex string
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]; //!+1nhi kiya kiuki 0th index bhi choose kr sakta hu
+    }
+    return color;
+}
+console.log(randomColor()); //!yes random color mil gya 
+// !add setinterval and it will automatically change background color
+let refernce;
+function startchanging() {
+    if (!refernce) {
+        refernce = setInterval(changecolor, 1000);
+    }   
+    function changecolor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+// !add clearInterval to stop changing the background color
+function stopchanging() {
+    clearInterval(refernce);
+    refernce = null; 
+}
+
+document.getElementById('start').addEventListener('click', startchanging);
+document.getElementById('stop').addEventListener('click', stopchanging);
